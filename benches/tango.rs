7adf9fe5b495c6e4ab7f8f6201b5643f9879e084
Denis Bazhenov <dotsid@gmail.com>
2023-10-04 13:58:17 +0700

    Subcommands added

--- benches/tango.rs
+++ benches/tango.rs
@@ -1,5 +1,5 @@
 // #![feature(fn_align)]
-
+use clap::Parser;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 use rust_pairwise_testing::{
     benchmark_fn, benchmark_fn_with_setup, reporting::ConsoleReporter, Benchmark, Generator,
@@ -39,7 +39,39 @@ fn copy_and_sort_stable(input: &Vec<u32>) -> usize {
     input.len()
 }
 
+#[derive(Parser, Debug)]
+enum RunMode {
+    Pair(PairOpts),
+    Calibration(CalibrationOpts),
+}
+
+#[derive(Parser, Debug)]
+struct PairOpts {
+    functions: Vec<String>,
+
+    #[arg(long = "bench", default_value_t = true)]
+    bench: bool,
+}
+
+#[derive(Parser, Debug)]
+struct CalibrationOpts {
+    #[arg(long = "bench", default_value_t = true)]
+    bench: bool,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    subcommand: RunMode,
+
+    #[arg(long = "bench", default_value_t = true)]
+    bench: bool,
+}
+
 fn main() {
+    let opts = Opts::parse();
+
     let mut benchmark = Benchmark::new(RandomVec(
         SmallRng::seed_from_u64(42),
         NonZeroUsize::new(100).unwrap(),
@@ -53,9 +85,21 @@ fn main() {
 
     let mut reporter = ConsoleReporter::default();
 
+    match opts.subcommand {
+        RunMode::Pair(opts) => {
+            assert!(opts.functions.len() >= 2);
+            let baseline = &opts.functions[0];
+            for candidate in &opts.functions[1..] {
+                benchmark.run_pair(baseline, candidate, &mut reporter);
+            }
+        }
+        RunMode::Calibration(_) => {
+            benchmark.run_calibration(&mut reporter);
+        }
+    }
+
     // benchmark.run_pair("stable", "unstable", &mut reporter);
 
-    benchmark.run_pair("stable", "unstable", &mut reporter);
-    benchmark.run_pair("stable", "copy_stable", &mut reporter);
-    benchmark.run_calibration(&mut reporter);
+    // benchmark.run_pair("stable", "unstable", &mut reporter);
+    // benchmark.run_pair("stable", "copy_stable", &mut reporter);
 }
