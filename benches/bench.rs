b09b2bfd34c4da7d7d9006b49e289ff99a23b476
Denis Bazhenov <dotsid@gmail.com>
2023-10-06 15:25:11 +0700

    sum tests added

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,11 +1,29 @@
+use std::hint::black_box;
+
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use rust_pairwise_testing::Generator;
 use test_funcs::{std_4925, std_5000, std_count, std_count_rev, RandomStringGenerator};
 
 mod test_funcs;
 
+fn sum(n: usize) -> usize {
+    let mut sum = 0;
+    for i in 0..black_box(n) {
+        sum += black_box(i);
+    }
+    sum
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("utf8");
+
+    group.bench_function("sum_50000", |b| {
+        b.iter(|| sum(50000));
+    });
+
+    group.bench_function("sum_49250", |b| {
+        b.iter(|| sum(49250));
+    });
 
     group.bench_function("std_length_4925", |b| {
         let mut generator = RandomStringGenerator::new().unwrap();
--- benches/tango-vec.rs
+++ benches/tango-vec.rs
@@ -2,7 +2,7 @@ use rand::{rngs::SmallRng, Rng, SeedableRng};
 use rust_pairwise_testing::{
     benchmark_fn, benchmark_fn_with_setup, cli::run, Benchmark, Generator,
 };
-use std::num::NonZeroUsize;
+use std::{hint::black_box, num::NonZeroUsize};
 
 struct RandomVec(SmallRng, NonZeroUsize);
 
@@ -37,17 +37,36 @@ fn copy_and_sort_stable(input: &Vec<u32>) -> usize {
     input.len()
 }
 
+fn sum_49250(_: &Vec<u32>) -> usize {
+    sum(49250)
+}
+
+fn sum_50000(_: &Vec<u32>) -> usize {
+    sum(50000)
+}
+
+fn sum(n: usize) -> usize {
+    let mut sum = 0;
+    for i in 0..black_box(n) {
+        sum += black_box(i);
+    }
+    sum
+}
+
 fn main() {
     let mut benchmark = Benchmark::new(RandomVec(
         SmallRng::seed_from_u64(42),
         NonZeroUsize::new(100).unwrap(),
     ));
 
-    benchmark.set_iterations(100000);
+    benchmark.set_iterations(10000);
 
     benchmark.add_function("stable", benchmark_fn_with_setup(sort_stable, Clone::clone));
     benchmark.add_function("copy_stable", benchmark_fn(copy_and_sort_stable));
     benchmark.add_function("unstable", benchmark_fn(sort_unstable));
+
+    benchmark.add_function("sum_50000", benchmark_fn(sum_50000));
+    benchmark.add_function("sum_49250", benchmark_fn(sum_49250));
 
     run(benchmark)
 }
