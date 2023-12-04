314281ea6abd3db14dc8ab1700e7613cf1c7c941
Denis Bazhenov <dotsid@gmail.com>
2023-12-04 10:40:26 +0700

    generators mod added with common used generators

---
+++ tango-bench/src/generators.rs
@@ -0,0 +1,31 @@
+use crate::Generator;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+pub struct RandomVec {
+    size: usize,
+    rng: SmallRng,
+}
+
+impl RandomVec {
+    pub fn new(size: usize) -> Self {
+        Self {
+            size,
+            rng: SmallRng::from_entropy(),
+        }
+    }
+}
+
+impl Generator for RandomVec {
+    type Haystack = Vec<u32>;
+    type Needle = ();
+
+    fn next_haystack(&mut self) -> Self::Haystack {
+        let mut v = vec![0; self.size];
+        self.rng.fill(&mut v[..]);
+        v
+    }
+
+    fn next_needle(&mut self, _: &Self::Haystack) -> Self::Needle {
+        todo!()
+    }
+}
