d6d854a9f3b75ed0a7cdc9c81c7e735a152d3df4
Denis Bazhenov <dotsid@gmail.com>
2023-12-19 11:15:28 +0700

    linear sampling script update

--- scripts/linear-sampling-test.sh
+++ scripts/linear-sampling-test.sh
@@ -6,8 +6,11 @@ cargo +nightly export ./target/benchmarks -- bench --bench='search-*'
 mkdir -p target/dump
 rm -f target/dump/*.csv
 
-for i in {1..100}; do
-    ./target/benchmarks/search_ord compare ./target/benchmarks/search_vec -t 1 -o \
-        -f 'search/u32/1024/nodup' -d target/dump --sampler=linear
-    mv "target/dump/search-u32-1024-nodup.csv" "target/dump/sample-$i.csv"
+for i in {1..30}; do
+    for sampler in flat linear random; do
+        printf "%10s : " "$sampler"
+        ./target/benchmarks/search_ord compare ./target/benchmarks/search_vec -t 1 \
+            -f 'search/u32/1024/nodup' -d target/dump --sampler="$sampler"
+        mv "target/dump/search-u32-1024-nodup.csv" "target/dump/$sampler-$i.csv"
+    done
 done
