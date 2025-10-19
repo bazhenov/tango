0fd5919435a7c14a9ec028f75c0d8c006304bebb
Denis Bazhenov <dotsid@gmail.com>
2025-10-19 17:06:48 +0200

    Dependency clarifications

--- examples/benches/threads.rs
+++ examples/benches/threads.rs
@@ -1,7 +1,7 @@
-use std::{hint::black_box, thread};
+use std::thread;
 use tango_bench::{benchmark_fn, tango_benchmarks, tango_main};
 
 tango_benchmarks!([benchmark_fn("spawn_join", |b| {
-    b.iter(|| thread::spawn(|| black_box(1)).join())
+    b.iter(|| thread::spawn(|| {}).join())
 })]);
 tango_main!();
