ed893c2ff53c84680514d5c33e6e89f85f32c95e
Denis Bazhenov <dotsid@gmail.com>
2026-03-14 18:42:18 +0100

    spawn instead of fork

--- tango-bench/tests/shmem.rs
+++ tango-bench/tests/shmem.rs
@@ -1,30 +1,37 @@
-use libc::{exit, fork};
 use shared_memory::ShmemConf;
 use std::{
+    env,
+    process::Command,
     sync::atomic::{AtomicU64, Ordering},
     time::{Duration, Instant},
 };
 
 fn main() {
-    let shmem = ShmemConf::new().size(4096).create().unwrap();
-    let os_name = shmem.get_os_id();
-    let pid = unsafe { fork() };
-    let shmem = if pid == 0 {
-        ShmemConf::new().os_id(os_name).open().unwrap()
+    let (shmem, child) = if let Ok(shmem_name) = env::var("SHMEM") {
+        let shmem = ShmemConf::new().os_id(shmem_name).open().unwrap();
+        (shmem, None)
     } else {
-        shmem
+        let shmem = ShmemConf::new().size(4096).create().unwrap();
+
+        let child = Command::new(env::current_exe().unwrap())
+            .env("SHMEM", shmem.get_os_id())
+            .spawn()
+            .unwrap();
+
+        (shmem, Some(child))
     };
 
     let shared = unsafe { &*(shmem.as_ptr() as *const AtomicU64) };
-    if shmem.is_owner() {
+    if let Some(child) = child {
         let start = Instant::now();
         let mut value = 0;
         while Instant::now().duration_since(start) < Duration::from_secs(10) && value == 0 {
             value = shared.load(Ordering::Acquire);
         }
         assert_eq!(value, 42);
+        child.wait_with_output().unwrap();
+        println!("Ok");
     } else {
         shared.store(42, Ordering::Release);
-        unsafe { exit(0) };
     }
 }
