3fcdac11ea1752d5757b127bc09c33a52e9d3887
Denis Bazhenov <dotsid@gmail.com>
2026-03-21 14:47:25 +0100

    Analyzer script

---
+++ tango-compare/analyze.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+WORKDIR=../target/benches
+cd "$WORKDIR"
+
+paste -d, \
+    <(cat "$WORKDIR/criterion.txt" | grep 'change:' | tail -n +2 | awk '{print $3}' | tr -d '%' | tr '−' '-') \
+    <(cat "$WORKDIR/criterion-in-place.txt" | grep 'change:' | tail -n +2 | awk '{print $3}' | tr -d '%' | tr '−' '-') \
+    <(cat "$WORKDIR/tango.txt" | awk '{print $9}' | tr -d '%*') > data.csv
+
+gnuplot criterion-vs-tango.gnuplot
---
+++ tango-compare/run.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [[ -f tango.txt ]]; then
+    rm tango.txt
+fi
+if [[ -f criterion.txt ]]; then
+    rm criterion.txt
+fi
+if [[ -f criterion-in-place.txt ]]; then
+    rm criterion.txt
+fi
+
+WORKDIR=./target/benches
+
+mkdir -p "$WORKDIR"
+
+# Exporting benchmarks
+cargo export "$WORKDIR" -- bench -p tango-compare --bench=criterion --bench=tango
+
+cd "$WORKDIR"
+
+# Copying the executable to be more fair. On macOS trying to load executable as .dylib will
+# lead to the same physical page mapped twice in VM of a process that will favor tango, because now
+# we're testing not the 2 copies of the same code, but actually the same code, which is not fair
+# to Criterion.
+cp ./tango ./tango-1
+cp ./tango ./tango-2
+
+# Saving baseline
+./criterion --save-baseline=main --bench -n > /dev/null
+
+while true; do
+    ./criterion --bench --baseline=main -n -v >> criterion.txt
+
+    ./criterion --save-baseline=in-place --bench -n > /dev/null
+    ./criterion --bench --baseline=in-place -n -v >> criterion-in-place.txt
+
+    ./tango-1 compare ./tango-2 -f 'str_length/random_limited' -t 1 -p >> tango.txt
+    sleep 10
+done
