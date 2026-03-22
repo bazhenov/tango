9c47567e581e0e61761551ae23a39eb8b6f1b0fe
Denis Bazhenov <dotsid@gmail.com>
2026-03-22 19:44:02 +0100

    Adding binary_search benchmark

--- tango-compare/run.sh
+++ tango-compare/run.sh
@@ -29,13 +29,13 @@ cp ./tango ./tango-1
 cp ./tango ./tango-2
 
 # Saving baseline
-./criterion --save-baseline=main --confidence-level=0.99 --bench -n > /dev/null
+./criterion --save-baseline=main --confidence-level=0.99 --bench -n binary_search > /dev/null
 
 while true; do
-    ./criterion --bench --baseline=main --confidence-level=0.99 --measurement-time=1 -n -v >> ./criterion.txt
+    ./criterion --bench --baseline=main --confidence-level=0.99 --measurement-time=1 -n -v binary_search >> ./criterion.txt
 
-    ./criterion --save-baseline=paired --confidence-level=0.99 --measurement-time=1 --bench -n > /dev/null
-    ./criterion --bench --baseline=paired --confidence-level=0.99 --measurement-time=1 -n -v >> ./criterion-paired.txt
+    ./criterion --save-baseline=paired --confidence-level=0.99 --measurement-time=1 --bench -n binary_search > /dev/null
+    ./criterion --bench --baseline=paired --confidence-level=0.99 --measurement-time=1 -n -v binary_search >> ./criterion-paired.txt
 
-    ./tango-1 compare ./tango-2 -t 1 -p --sampler=flat >> ./tango.txt
+    ./tango-1 compare ./tango-2 -t 1 -p -f binary_search >> ./tango.txt
 done
