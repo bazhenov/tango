a2861feef39d1c1612532ea996dcf870359d1f67
Denis Bazhenov <dotsid@gmail.com>
2026-03-31 20:08:45 +0200

    typo

--- tango-test/build.rs
+++ tango-test/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Using `rustc-link-arg` instead of `rustc-link-arg-benches`,
-    // becase we build benchmarks from the main executable
+    // because we build benchmarks from the main executable
     println!("cargo:rustc-link-arg=-rdynamic");
     println!("cargo:rerun-if-changed=build.rs");
 }
