40ed2585b75d356e2cc2da06d10bb57d9ec221cc
Denis Bazhenov <dotsid@gmail.com>
2026-05-02 14:28:44 +0200

    Reading DMI name

--- tango-compare/terraform/run.sh
+++ tango-compare/terraform/run.sh
@@ -15,14 +15,18 @@ mkdir result
 cat /proc/cpuinfo > result/cpuinfo
 hostname > result/hostname
 free > result/free
+# reading DMI product name (for AWS this is instance type eg. t4g.small)
+if [[ -f /sys/devices/virtual/dmi/id/product_name ]]; then
+    cat /sys/devices/virtual/dmi/id/product_name > result/aws_instance_type
+fi
 
 tar czvf result.tar.gz result/
 aws s3 cp result.tar.gz "s3://${s3_bucket_name}/$UUID.tar.gz"
 
 for i in $(seq 1 1000);
 do
-    (./tango_slower-commpage compare -f factorial -t 1 || true) >> result/commpage.txt
-    (./tango_slower-main compare -f factorial --sampler flat -t 1 -p || true) >> result/main.txt
+    (./tango_slower-commpage compare -f factorial -t 2 || true) >> result/commpage.txt
+    (./tango_slower-main compare -f factorial --sampler flat -t 2 -p || true) >> result/main.txt
 done
 
 tar czvf result.tar.gz result/
